460462f3d8816b899ddaf4ea98b334998ff13c86
Chinmay Mandale <chinmaymandale@gmail.com>
2022-09-24 19:14:05 -0500

    main components & toolbar + tabs

---
+++ solgram/src/app/analyze/analyze.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-analyze',
+  templateUrl: './analyze.component.html',
+  styleUrls: ['./analyze.component.scss']
+})
+export class AnalyzeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- solgram/src/app/app.module.ts
+++ solgram/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+import { AnalyzeComponent } from './analyze/analyze.component';
+import { ResultsComponent } from './results/results.component';
+import { MetricsComponent } from './metrics/metrics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnalyzeComponent,
+    ResultsComponent,
+    MetricsComponent
   ],
   imports: [
+    MatTabsModule,
+    MatToolbarModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
