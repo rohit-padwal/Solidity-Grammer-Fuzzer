5a045481b00f91e8d549bb2174ac2d189314dfa3
Chinmay Mandale <chinmaymandale@gmail.com>
2022-09-26 19:46:27 -0500

    upload file complete

--- solgram/src/app/analyze/analyze.component.ts
+++ solgram/src/app/analyze/analyze.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-analyze',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnalyzeComponent implements OnInit {
 
-  constructor() { }
+  fileName: string = 'Select File';
+
+  constructor(private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
 
+  uploadFile(fileName: string) {
+    if(fileName !== 'Select File') {
+      this.snackBar.open(`${fileName} uploaded successfully.`, '', {
+        duration:2000
+      });
+    }
+  }
+
+  selectFile(event: any): void {
+    if (event.target.files && event.target.files[0]) {
+      const file: File = event.target.files[0];
+      this.fileName = file.name;
+    } else {
+      this.fileName = 'Select File';
+    }
+  }
 }
