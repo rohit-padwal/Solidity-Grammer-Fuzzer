d19ed187cc48132ac4ce975e22079ea253b897ff
Sanjoli <sxs3872@mavs.uta.edu>
2022-11-28 18:42:04 -0600

    Updated response of parsed solhint output
    Updated response of parsed solhint output

--- server/parse_solhint_output.py
+++ server/parse_solhint_output.py
@@ -7,20 +7,27 @@ def parse_output(output):
     string_output = output.decode()
     arr = string_output.split("\n")
     output_dict = dict()
+    output_dict["response"] = list()
     current_key = ""
+    new_dict = dict()
     for line in arr:
         line = line.strip()
         found = False
         if not line == "":
             if ".sol" in line:
+                if current_key is not "":
+                    output_dict["response"].append(new_dict)
                 current_key = line
-                output_dict[line] = list()
+                new_dict = dict()
+                new_dict["contractName"] = current_key
+                new_dict["errorResponse"] = list()
             else:
                 for key in solgram_mitigations.keys():
                     if key in line:
-                        output_dict[current_key].append(line + ". " + solgram_mitigations[key])
+                        new_dict["errorResponse"].append(line + ". " + solgram_mitigations[key])
                         found = True
                         break
                 if not found:
-                    output_dict[current_key].append(line)
+                    new_dict["errorResponse"].append(line)
+    output_dict["response"].append(new_dict)
     return output_dict
